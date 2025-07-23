a9779ec22045ea8849894a2776d2e8a269606613
srsgh <theshrutirawat@gmail.com>
2025-07-23 07:13:42 +0530

    angularjs controller vm added

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 (function () {
   //angular.module(apps_name , dependency_array)- HTML is bound to it via ng-app="my_first_ng_app"
-  angular.module("my_first_ng_app", []);
+  angular
+    .module("my_first_ng_app", [])
+    .controller("my_first_controller", function () {});
+  //.controller(controllers_name,controllers_functionality) ~ View Model - HTML binds to VM/Controller using ng-controller="controllers_name"
 })();
