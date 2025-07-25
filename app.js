b17f179209e9333cb8a59ac59ddead5f7b0e412e
srsgh <theshrutirawat@gmail.com>
2025-07-26 03:34:00 +0530

    Injection/Inversion of Control: , , ng-blur, ng-modal

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 (function () {
   "use strict";
   //angular injector
-  angular
-    .module("ngapp2", [])
-    .controller("controller2", function ($scope, $filter) {
-      $scope.name = "ShibYu";
-      $scope.upper = function () {
-        var upCase = $filter("uppercase");
-        $scope.name = upCase($scope.name);
-      };
-    });
+  angular.module("ngapp2", []).controller("controller2", Bye);
+  function Bye($scope, $filter) {
+    $scope.name = "ShibYu";
+    $scope.upper = function () {
+      var upcase = $filter("uppercase");
+      $scope.name = upcase($scope.name);
+    };
+  }
   //notes end
 })();
