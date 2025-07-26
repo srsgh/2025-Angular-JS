62329f262b565132c6c92e070fef2a4df54e7f8e
srsgh <theshrutirawat@gmail.com>
2025-07-26 05:49:15 +0530

    filter in js and html for angular (filtername,args.....) and {{expression|filtername:arg1:arg2:...}}

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 (function () {
   "use strict";
   //angular injector
-  angular.module("ngapp2", []).controller("controller2", Bye);
-  function Bye($scope, $filter) {
+  angular.module("ngapp2", []).controller("controller2", ControllerFn);
+
+  //Below Line is for Minification: -
+  // ControllerFn.$injector = ["$scope", "$filter"];
+  function ControllerFn($scope, $filter, $injector) {
     $scope.name = "ShibYu";
+    $scope.name2 = "DoberMAn";
     $scope.upper = function () {
-      var upcase = $filter("uppercase");
-      $scope.name = upcase($scope.name);
+      //$filter('upperacase')
+      // var upcase = $filter("uppercase");
+      // $scope.name = upcase($scope.name);
+      $scope.name = $filter("uppercase")($scope.name);
     };
+    //notes end- $injector.annnotate heps pick services - i.e. $xxx up in a contorller fn
+    console.log($injector.annotate(ControllerFn));
   }
-  //notes end
 })();
