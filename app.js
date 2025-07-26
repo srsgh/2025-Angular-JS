2ad9ec63882a1e89bdb4a9f6039e05cd347e95ef
srsgh <theshrutirawat@gmail.com>
2025-07-26 19:41:19 +0530

    This is Calc: Using ng-repeat and rest of the stuff but use directive ng-repeat to get list items for angjs contetx

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 (function () {
-  "use strict";
-  //This is going to use $scope.$watch & Digest cycle conecpt $digest, for every change digest cycle runs atleas twice over the watchers
-  angular.module("ngapp3", []).controller("controller3", Controller3);
-  Controller3.$inject = ["$scope"];
-  function Controller3($scope) {
-    $scope.var1 = 0;
-    console.log("Lets see $scope");
-    console.log($scope);
-
-    $scope.getScopeService = function () {
-      console.log("Lets see $scope");
-      console.log($scope.$$watchersCount);
+  angular
+    .module("ngapprepeat", [])
+    .controller("controllerRepeat", ControllerRepeat);
+  ControllerRepeat.$inject = ["$scope"];
+  function ControllerRepeat($scope) {
+    $scope.history = [];
+    $scope.num1 = 0;
+    $scope.num2 = 0;
+    $scope.op = "";
+    $scope.output = 0;
+    $scope.status = "Status: ";
+    $scope.add = function () {
+      $scope.op = "+";
+      $scope.output = $scope.num1 + $scope.num2;
     };
-
-    $scope.setVar1 = function () {
-      $scope.var1 = 1;
-      console.log("Lets see $scope");
-      //   console.log($scope.$$watchersCount);
+    $scope.sub = function () {
+      $scope.op = "-";
+      $scope.output = $scope.num1 - $scope.num2;
     };
-    //set up a watch for var1 and add a function for new and old values
-    $scope.$watch("var1", function (newVal, oldVal) {
-      console.log("newVal:" + newVal);
-      console.log("oldVal:" + oldVal);
-    });
-
-    $scope.IncVar1 = function () {
-      $scope.var1 = $scope.var1 + 1;
-      setTimeout(function () {
-        console.log(" delay increment");
-        $scope.var1 = $scope.var1 + 10;
-        //since seTimeout is non angjs we need $digest to be called for rendering the UI for it
-        $scope.$digest();
-      }, 3000);
-      //   console.log($scope.$$watchersCount);
+    $scope.mul = function () {
+      $scope.op = "*";
+      $scope.output = $scope.num1 * $scope.num2;
+    };
+    $scope.div = function () {
+      $scope.op = "/";
+      $scope.output = $scope.num1 / $scope.num2;
+    };
+    $scope.save = function () {
+      var temp = {};
+      temp.num1 = $scope.num1;
+      temp.num2 = $scope.num2;
+      temp.num2 = $scope.num2;
+      temp.op = $scope.op;
+      temp.output = $scope.output;
+      $scope.history.push(temp);
+      $scope.status = "Added to history";
+    };
+    $scope.viewHistory = function () {
+      $scope.historyView = $scope.history;
+      $scope.status = "Show history";
+    };
+    $scope.hideHistory = function () {
+      $scope.historyView = "";
+      $scope.status = "Hide history";
     };
   }
 })();
---
+++ three/app3.js
@@ -0,0 +1,50 @@
+(function () {
+  "use strict";
+  //This is going to use $scope.$watch & Digest cycle conecpt $digest, for every change digest cycle runs atleas twice over the watchers
+  angular.module("ngapp3", []).controller("controller3", Controller3);
+  Controller3.$inject = ["$scope"];
+  function Controller3($scope) {
+    $scope.var1 = 0;
+    console.log("Lets see $scope");
+    console.log($scope);
+
+    $scope.getScopeService = function () {
+      console.log("Lets see $scope");
+      console.log($scope.$$watchersCount);
+    };
+
+    $scope.setVar1 = function () {
+      $scope.var1 = 1;
+      console.log("Lets see $scope");
+      //   console.log($scope.$$watchersCount);
+    };
+    //set up a watch for var1 and add a function for new and old values
+    $scope.$watch("var1", function (newVal, oldVal) {
+      console.log("newVal:" + newVal);
+      console.log("oldVal:" + oldVal);
+    });
+
+    $scope.IncVar1 = function () {
+      $scope.var1 = $scope.var1 + 1;
+      /*
+      //wai 1: this lacks EH
+      setTimeout(function () {
+        console.log(" delay increment");
+        $scope.var1 = $scope.var1 + 10;
+        //since seTimeout is non angjs we need $digest to be called for rendering the UI for it
+        $scope.$digest();
+      }, 3000);
+      //   console.log($scope.$$watchersCount);
+    };
+    */
+      //way 2: has EH-> calls $scope.$giest on its own in the end
+      setTimeout(function () {
+        $scope.$apply(function () {
+          console.log(" delay increment");
+          $scope.var1 = $scope.var1 + 10;
+          //since seTimeout is non angjs we need $digest to be called for rendering the UI for it
+        });
+      }, 5000);
+    };
+  }
+})();
