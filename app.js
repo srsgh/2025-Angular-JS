26124cc6139c4461b64b4c7e2846adb673817c72
srsgh <theshrutirawat@gmail.com>
2025-07-26 20:15:37 +0530

    bleh

--- app.js
+++ app.js
@@ -5,26 +5,56 @@
   ControllerRepeat.$inject = ["$scope"];
   function ControllerRepeat($scope) {
     $scope.history = [];
+    $scope.fullHistory = [];
     $scope.num1 = 0;
     $scope.num2 = 0;
     $scope.op = "";
+    $scope.operator = "";
     $scope.output = 0;
     $scope.status = "Status: ";
     $scope.add = function () {
       $scope.op = "+";
       $scope.output = $scope.num1 + $scope.num2;
+      var temp1 = {};
+      temp1.num1 = $scope.num1;
+      temp1.num2 = $scope.num2;
+      temp1.num2 = $scope.num2;
+      temp1.op = $scope.op;
+      temp1.output = $scope.output;
+      $scope.fullHistory.push(temp1);
     };
     $scope.sub = function () {
       $scope.op = "-";
       $scope.output = $scope.num1 - $scope.num2;
+      var temp1 = {};
+      temp1.num1 = $scope.num1;
+      temp1.num2 = $scope.num2;
+      temp1.num2 = $scope.num2;
+      temp1.op = $scope.op;
+      temp1.output = $scope.output;
+      $scope.fullHistory.push(temp1);
     };
     $scope.mul = function () {
       $scope.op = "*";
       $scope.output = $scope.num1 * $scope.num2;
+      var temp1 = {};
+      temp1.num1 = $scope.num1;
+      temp1.num2 = $scope.num2;
+      temp1.num2 = $scope.num2;
+      temp1.op = $scope.op;
+      temp1.output = $scope.output;
+      $scope.fullHistory.push(temp1);
     };
     $scope.div = function () {
       $scope.op = "/";
       $scope.output = $scope.num1 / $scope.num2;
+      var temp1 = {};
+      temp1.num1 = $scope.num1;
+      temp1.num2 = $scope.num2;
+      temp1.num2 = $scope.num2;
+      temp1.op = $scope.op;
+      temp1.output = $scope.output;
+      $scope.fullHistory.push(temp1);
     };
     $scope.save = function () {
       var temp = {};
@@ -38,11 +68,19 @@
     };
     $scope.viewHistory = function () {
       $scope.historyView = $scope.history;
-      $scope.status = "Show history";
+      $scope.status = "Show Saved history";
+    };
+    $scope.showFullHistory = function () {
+      $scope.fullHistoryView = $scope.fullHistory;
+      $scope.status = "Show Full history";
     };
     $scope.hideHistory = function () {
       $scope.historyView = "";
       $scope.status = "Hide history";
+    };
+    $scope.hideFullHistory = function () {
+      $scope.fullHistoryView = "";
+      $scope.status = "Hide Full history";
     };
   }
 })();
