a48ba6deb3583f920df8167eb0dc78b585fa0d28
srsgh <theshrutirawat@gmail.com>
2025-08-04 07:31:28 +0530

    Controller as syntax:  has prototypal inheritence. child can mask properties of parent so we use this to prevent that and still be able to access parernt's props with child in DOMs

--- app.js
+++ app.js
@@ -7,13 +7,13 @@
   ControllerFn1.$inject = ["$scope"];
   function ControllerFn1($scope) {
     //any property on this is accessible by parent from html
-    var controller1 = this;
-    controller1.value = "This is value for controller 1";
+    var parent = this;
+    parent.value = "This is value for controller 1";
   }
   ControllerFn2.$inject = ["$scope"];
   function ControllerFn2($scope) {
     //any property on this is accessible by child from html
-    var controller2 = this;
-    controller2.value = "This is value for controller 2";
+    var child = this;
+    child.value = "This is value for controller 2";
   }
 })();
